188785aabe84998a99302abb65714987d0a232cc
Roshan Kumar Sharma <roshan.rks2812000@gmail.com>
2024-03-13 23:55:30 +0530

    * Created Sign up page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Signup from './Component/Signup'
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Container } from 'react-bootstrap'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Container className='d-flex justify-content-center align-items-center' style={{minHeight: "100vh"}}>
+      <div className='w-100' style={{maxWidth: "400px"}}>
+      <Signup/>  
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </Container>    
     </>
   )
 }
---
+++ src/Component/Signup.jsx
@@ -0,0 +1,47 @@
+import React, { useRef, useState } from "react"
+import { Form, Button, Card, Alert } from "react-bootstrap"
+
+export default function Signup() {
+
+  const [error, setError] = useState("")
+  const [loading, setLoading] = useState(false)
+  const emailRef = useRef()
+  const passwordRef = useRef()
+  const passwordConfirmRef = useRef()
+
+ const handleSubmit = () => {
+
+ }
+
+  return (
+    <>
+      {" "}
+      <Card>
+        <Card.Body>
+          <h2 className="text-center mb-4 w-full">Sign Up</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="text-left" id="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" ref={emailRef} required />
+            </Form.Group>
+            <Form.Group className="text-left" id="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" ref={passwordRef} required />
+            </Form.Group>
+            <Form.Group className="text-left" id="password-confirm">
+              <Form.Label>Password Confirmation</Form.Label>
+              <Form.Control type="password" ref={passwordConfirmRef} required />
+            </Form.Group>
+            <Button disabled={loading} className="w-100" type="submit">
+              Sign Up
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+      <div className="w-100 text-center mt-2">
+        Already have an account? Login
+      </div>
+    </>
+  )
+}

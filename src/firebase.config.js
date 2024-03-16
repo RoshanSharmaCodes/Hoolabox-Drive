f4aedb25cb8889951813761830ab2b4ae75c12dc
Roshan Kr. Sharma <90642401+RoshanSharmaCodes@users.noreply.github.com>
2024-03-16 12:19:05 +0530

    Update firebase.config.js

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -2,12 +2,12 @@ import firebase from "firebase/app"
 import "firebase/auth"
 
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyC1FRsfy9WzCaVmdYtoVgt1ztHlMfRmm4E",
+  apiKey: "",
   authDomain: "hoolabox-drive.firebaseapp.com",
   projectId: "hoolabox-drive",
   storageBucket: "hoolabox-drive.appspot.com",
   messagingSenderId: "437411372340",
-  appId: "1:437411372340:web:372751f43652bcb55c9961"
+  appId: ""
 
 })
 
